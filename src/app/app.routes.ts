0f4bb8528610b1d327ab5524b0ae6dab3d650d67
Joaquim <joaquimoliart123@gmail.com>
2025-08-19 20:43:07 -0300

    Commit component e pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.routes')
+        .then(rota => rota.routes)
+    }
+];
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,20 @@
 export const environment = {
   production: false
 };
+
+export const Spotifyenvironment = {
+  clientID: "4603d931ef8244d5bc9fd88f69217415",
+  authEndpoint:"https://accounts.spotify.com/authorize",
+  apiTokenEndpoint: "https://accounts.spotify.com/api/token",
+  redirectUrl: "http://127.0.0.1/login",
+   scopes: [
+    'user-read-currently-playing', //Musica que esta tocando
+    'user-read-recently-played', //Musicas que foram tocadas recentemente
+    'user-read-playback-state', //Estado da reproduçao
+    'user-top-read', //Top artistas e musicas
+    'user-modify-playback-state', //Controlar a reproduçao
+    'user-library-read', //Ler biblioteca de musicas
+    'playlist-read-private', //Ler playlists privadas
+    'playlist-read-collaborative' //Ler playlists colaborativas
+  ]
+}
