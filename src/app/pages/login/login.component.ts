4ef0341f439470940e18777e47776b69b5d26476
Joaquim <joaquimoliart123@gmail.com>
2025-09-02 19:27:16 -0300

    Commit: erro no environment

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
-import { SpotifyService } from "../../services/spotify.service";
+
+import {ChangeDetectionStrategy, Component, inject, OnInit} from "@angular/core";
+import {SpotifyService} from "../../services/spotify.service";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +10,32 @@ import { SpotifyService } from "../../services/spotify.service";
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  roteador = inject(Router);
+
+  ngOnInit(): void {
+    this.verficarCodigoUrlCallback();
+  }
 
   serviceSpotify = inject(SpotifyService);
   anoAtual = new Date().getFullYear();
 
   async fazerLogin() {
     const url = await this.serviceSpotify.obterUrlLogin();
+    window.location.href = url;
+  }
+
+  async verficarCodigoUrlCallback() {
+    const params = new URLSearchParams(window.location.search);
+    const codigo = params.get("code");
+
+    if (codigo) {
+      const sucesso = await this.serviceSpotify.definirAcesstoken(codigo);
+
+      if (sucesso) {
+        this.roteador.navigate(['/player']);
+      }
+    }
   }
 }
