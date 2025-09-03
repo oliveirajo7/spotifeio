ca5413fbcbc2536e602f6c96c486ef7f9774b372
Joaquim <joaquimoliart123@gmail.com>
2025-09-02 21:39:10 -0300

    Feat: player

--- src/app/components/painel-esquerdo/painel-esquerdo.component.ts
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -1,14 +1,24 @@
 import {ChangeDetectionStrategy, Component, OnInit} from "@angular/core";
+import {BotaoMenuComponent} from '../botao-menu/botao-menu.component';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faHome, faSearch, faUser} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-painel-esquerdo',
   standalone: true,
-  imports: [],
+  imports: [
+    BotaoMenuComponent,
+    FaIconComponent
+  ],
   templateUrl: './painel-esquerdo.component.html',
   styleUrl: './painel-esquerdo.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PainelEsquerdoComponent implements OnInit {
+
+  faHome = faHome;
+  faSearch = faSearch;
+  faUser = faUser;
 
   ngOnInit(): void {
 
