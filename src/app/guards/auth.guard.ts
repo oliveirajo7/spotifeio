8b756e06c9d9ffe6b40734b48cd2826db4d720a0
Joaquim <joaquimoliart123@gmail.com>
2025-09-02 21:52:20 -0300

    feat: scss do botao

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,18 +1,18 @@
-import {CanActivateFn, Router, UrlTree} from '@angular/router';
-import {inject} from '@angular/core';
+import { inject } from "@angular/core";
+import { CanActivateFn, Router, UrlTree } from "@angular/router";
 
-export const authGuard: CanActivateFn =
-  async (rota, estado):
-    Promise<boolean | UrlTree> => {
+export const authGuard: CanActivateFn = async (rota, estado): Promise<boolean | UrlTree> => {
 
-  const roteador = inject(Router);
-  const accessToken = localStorage.getItem('access_token');
-  if (!accessToken) {
-    alert('Acesso negado. Por favor, faça login.');
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('code_verifier');
-    return roteador.parseUrl('/login');
-  }
+    const roteador = inject(Router);
+    const access_token = localStorage.getItem('access_token');
+    if (!access_token) {
+        alert('Usuário não autenticado. Por favor, faça login.');
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('code_verifier');
+        return roteador.parseUrl('/login');
+    }
 
-  return true;
+
+    return true;
+
 }
